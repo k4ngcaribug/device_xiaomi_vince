2eec02200148504243a44b8e34842f26244e1ae8
k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>
2024-10-27 00:10:52 +0000

    vince: Remove Dolby atmos
    Signed-off-by: k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,8 +97,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
-    vendor/afterlife/config/device_framework_matrix.xml \
-    $(DEVICE_PATH)/vintf/dolby_compat_framework_matrix.xml
+    vendor/afterlife/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/vintf/framework_manifest.xml
