48ad3c9bcfaf53eeca4634796d9a80bae26ff009
k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>
2024-11-02 03:59:32 +0000

    vince: Remove thermal-engine
    Signed-off-by: k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -373,10 +373,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.c_shim@1.2 \
     android.hardware.radio.config@1.1-service.wrapper
 
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic
